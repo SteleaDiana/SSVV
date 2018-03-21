c944c6886b737a1995c979d109e28381a49c58d4
Stelea\Diana <diana_stelea@yahoo.com>
2018-03-21 14:56:13 +0200

    lab2

---
+++ src/main/java/validator/Validator.java
@@ -0,0 +1,47 @@
+package validator;
+
+import model.Laboratory;
+import model.Student;
+
+import java.util.Date;
+
+public class Validator {
+
+    public static boolean validateStudent(Student student) {
+        if(!student.getRegNumber().matches("[a-zA-Z]{4}[\\d]{4}")){
+            System.out.println("Wrong Format!");
+            return false;
+        }
+        if (!student.getName().matches("[a-zA-Z]+[\\s]?[a-zA-Z]+")) {
+            System.out.println("Wrong Format!");
+            return false;
+        }
+        if(student.getGroup() >= 1000 || student.getGroup() <= 0){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validateLaboratory(Laboratory laboratory) {
+        if(laboratory.getLaboratoryNumber() < 1) {
+            System.out.println("The lab number should be > 1!");
+            return false;
+        }
+        if(laboratory.getProblemNumber() > 10 || laboratory.getProblemNumber() < 1) {
+            System.out.println("The problem number should be between 1 and 10!");
+            return false;
+        }
+        Date date = new Date();
+        if(date.before(laboratory.getDate())) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validateGrade(float grade) {
+        if(grade >= 1 && grade <= 10) {
+            return true;
+        }
+        return false;
+    }
+} 
